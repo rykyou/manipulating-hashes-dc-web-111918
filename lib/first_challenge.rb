2dad4fc2d110ffb1175596a4d21cf8ce3670a414
Robin Kim <rkim1214@gmail.com>
2018-10-25 21:23:29 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,16 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
+  contacts.each do |person, data|
+    if person == "Freddy Mercury"
+      data.each do |attribute, value|
+        if attribute == :favorite_icecream_flavors
+          value.delete_if { |x| x == "strawberry" }
+        end 
+      end 
+    end 
+  end 
+  
   contacts
 end
 
